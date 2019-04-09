e0ddf902eb1cd0cc35017ad771ac7e5e64dd1557
Sayan Bit <sayan.bit@hpe.com>
2019-04-09 14:53:55 +0530

    Changes Done

--- routes/activity.js
+++ routes/activity.js
@@ -72,7 +72,7 @@ exports.save = function (req, res) {
  * POST Handler for /execute/ route of Activity.
  */
 exports.execute = function (req, res) {
-app.post("==inexecute");
+console.log("==inexecute");
     // example on how to decode JWT
     JWT(req.body, process.env.jwtSecret, (err, decoded) => {
 

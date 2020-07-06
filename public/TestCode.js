e9098bc4ebbb03330b2e3052eba009a7aefccf32
lahassija <67906824+lahassija@users.noreply.github.com>
2020-07-06 20:29:37 +0530

    Update TestCode.js

--- public/TestCode.js
+++ public/TestCode.js
@@ -1,7 +1,7 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: 'sayanbit8@gmail.com',
+  to: 'lavinahassija@gmail.com',
   from: 'test@example.com',
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
